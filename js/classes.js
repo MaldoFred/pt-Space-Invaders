9f034e4d4ecf3c3c0d0771c9d098183e06c40817
MaldoFred <114544297+MaldoFred@users.noreply.github.com>
2022-11-06 01:15:25 -0600

    pinta jugador y enemigos con movimiento

--- js/classes.js
+++ js/classes.js
@@ -3,22 +3,93 @@
 class Background{
     constructor(w,h){
      //Dibujar en canvas x,y,w,h 
-     this.x =10;
-     this.y =10;
-     this.width = w;
-     this.height = h;
+     this.x =0;
+     this.y =0;
+     this.width = canvas.width;
+     this.height = canvas.height;
      //image
      //imagen de fondo
      this.image = new Image();
-        this.image.src = "../images/bg.jpg";
+        this.image.src = "images/bg.jpg";
 
     }
 
     update(){
-        console.log("update");
+    
         
 
-        ctx.drawImage(this.image,this.x,this.y);
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
 
     }
 }
+
+//spacecraft
+
+class Spacecraft1{
+    constructor (x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.image = new Image();
+        this.image.src = "images/spacecraft1.png"
+        
+    }
+    
+    update(){
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+    }
+
+
+    collistion(){}
+}
+// let spacecraft1=document.querySelector(".spacecraft1")
+//         let body=document.querySelector("body")
+//         document.addEventListener("mousemove",(e)=>{
+//             spacecraft1.style.left=e.clientX+"px";
+//         })
+
+// Enemys 1
+
+class Enemy1{
+    constructor (x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.image = new Image();
+        this.image.src = "images/enemy1.png"
+        
+    }
+    
+    update(){
+        ctx.drawImage(this.image,this.x,this.y+=.3,this.width,this.height)
+    }
+
+
+    collistion(){}
+}
+
+// Enemys 1
+
+class Enemy2{
+    constructor (x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.image = new Image();
+        this.image.src = "images/enemy2.png"
+        
+    }
+    
+    update(){
+        ctx.drawImage(this.image,this.x,this.y+=.3,this.width,this.height)
+    }
+
+
+    collistion(){}
+}
+    
+
+    
--- js/script.js
+++ js/script.js
@@ -4,12 +4,18 @@ window.onload = function() {
     // var ctx = document.getElementById("my-canvas").getContext("2d");
     //llamar clase
     const bg = new Background(canvas.widht,canvas.height);
-    startGame();
+    const spacecraft1 = new Spacecraft1(300,500,80,80);
+    const enemy1 = new Enemy1(300,-80,80,80);
+    const enemy2 = new Enemy2(100,-80,80,80);
+
+
+    
     document.getElementById("start-button").onclick = function() {
-     
+        startGame();
+        setInterval(updateGame,6);
     };
   
-    function startGame() {
+    function startGame() {1
         
 
         //ejecuta updateGame
@@ -18,10 +24,52 @@ window.onload = function() {
     }
 
     function updateGame(){ // actualizar juego
+ctx.clearRect(0,0,canvas.width,canvas.height);
+        bg.update();
 
-        bg.update()
-
+        spacecraft1.update();
+        enemy1.update();
+        enemy2.update();
     }
-  
+
+
+    // addEventListener 
+    //addEventListener()
+  this.addEventListener("keydown", function(event){
+    if(event.keyCode == '37'){//Tecla con la flecha hacia la izquierda		
+        //alert('izquierda');
+        console.log(spacecraft1.x)
+
+        if(spacecraft1.x>=0){
+
+        spacecraft1.x-=6;
+        }
+     }
+
+     if(event.keyCode == '39'){ //Tecla con la flecha hacia la Derecha					
+       // alert('derecha =)');
+       console.log(spacecraft1.x)
+       if(spacecraft1.x<=canvas.width-80){
+        spacecraft1.x+=6;
+
+       
+       }
+     }
+     if(event.keyCode == '38'){//Tecla con la flecha hacia arriba		
+       // alert('arriba');
+       if(spacecraft1.y>=0){
+
+       spacecraft1.y-=6;
+       }
+     }
+     if(event.keyCode == '40'){ //Tecla con la flecha hacia abajo					
+        //alert('abajo =)');
+        if(spacecraft1.y<=canvas.height-80){
+
+        spacecraft1.y+=6;
+        }
+     }
+    console.log(event.keyCode);
+  })
   };
   
